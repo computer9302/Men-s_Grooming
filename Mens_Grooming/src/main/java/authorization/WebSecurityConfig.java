f5ebc076fd4a0b6886a78370498894a8084b96d3
computer9302 <computer9302@gmail.com>
2024-04-11 22:50:55 +0900

    OAuth In-memory 방식 구현 ClassNotFoundException

--- Mens_Grooming/src/main/java/authorization/WebSecurityConfig.java
+++ Mens_Grooming/src/main/java/authorization/WebSecurityConfig.java
@@ -1,15 +1,20 @@
 package authorization;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import lombok.AllArgsConstructor;
 
@@ -34,14 +39,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		}
 		
 		@Bean
-		public PasswordEncoder passwordEncoder() {
+		public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
 			return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		}
 		
 		@Bean
 		public UserDetailsService userDetailsService() {
-			PasswordEncoder encoder = passwordEncoder();
+			org.springframework.security.crypto.password.PasswordEncoder encoder = passwordEncoder();
 			String password = encoder.encode("pass");
+			log.debug("PasswordEncoder password : [{}] ", password);
+			log.debug("PasswordEncoder password : [{}]", encoder.encode("secret"));
 			
+			InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+			manager.createUser(User.withUsername("user").password(password).roles("USER").build());
+			manager.createUser(User.withUsername("admin").password("{nop}pass").roles("USER", "ADMIN").build());
+			
+			return manager;
+		}
+		
+		private static void makeAuthorizationRequestHeader() {
+			String oauthClientId = "client";
+			String oauthClientSecret = "secret";
+			
+			Encoder encoder = Base64.getEncoder();
+			try {
+				String toEncodeString = String.format("%s:%s", oauthClientId, oauthClientSecret);
+				String authorizationRequestHeader = "Basic " + encoder.encodeToString(toEncodeString.getBytes("UTF-8"));
+				log.debug("AutorizationRequestHeader : [{}]", authorizationRequestHeader);
+			} catch (UnsupportedEncodingException e) {
+				// TODO: handle exception
+				log.error(e.getMessage(), e);
+			}
 		}
 }
